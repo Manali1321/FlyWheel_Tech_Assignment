83a771a36e62b369e39466aa0a3d524c4f5335f0
Manali Patel <113155434+Manali1321@users.noreply.github.com>
2023-06-07 19:57:29 -0400

    Update q1.js

--- Question1/q1.js
+++ Question1/q1.js
@@ -1,12 +1,18 @@
+// On window load function will run
 window.onload = function () {
+//   create a for loop to show 1 to 100 number
   for (var i = 1; i <= 100; i++) {
+    //       if number is multiples of 4 and 3 show fizzBuzz
     if (i % 4 === 0 && i % 3 === 0) {
       console.log("FizzBuzz");
     } else if (i % 4 === 0) {
+      //       if number is multiples of 4 show Fizz
       console.log("Fizz");
     } else if (i % 3 === 0) {
+      //       if number is multiples of 3 show Buzz
       console.log("Buzz");
     } else {
+//       If number is not multiple of 3 or 4 shows number
       console.log(i);
     }
   }
